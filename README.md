78d25ea4907cb9aae7a3f39b3f6e73d4a5fb3505
HiCoders <hicodersofficial@gmail.com>
2022-02-11 18:27:40 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Image processing API
+
+```
+$ npm install
+```
+
+```
+http://localhost:5000/public/{filename}?q=90&s=200&fit=inside
+```
+
+## **Screenshots**
+
+**Original** (1.5 MB) &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp; **Compressed**(75 KB)
+
+<div>
+<img src="./docs/original-img.jpg" width="50%"><img src="./docs/original-img-com.jpg" width="50%">
+</div>
+
+**Original** (3 MB) &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp; **Compressed**(12 KB)
+
+<div>
+<img src="./docs/original-img2.jpg"width="50%" ><img src="./docs/original-img1-com.jpg"width="50%">
+</div>
---
+++ index.js
@@ -0,0 +1,128 @@
+const express = require("express");
+const fs = require("fs");
+const multer = require("multer");
+const path = require("path");
+const sharp = require("sharp");
+const app = express();
+const { v4 } = require("uuid");
+
+// custom logger (Alt: morgan)
+app.use((req, res, next) => {
+  console.log(req.ip, req.method, req.path, req.headers["user-agent"]);
+  next();
+});
+
+app.use(express.json());
+
+app.use("/public/:filename", async (req, res, next) => {
+  try {
+    const { filename } = req.params;
+    let { w, h, q, fit, bg, s } = req.query;
+
+    if (s) {
+      s = parseInt(s);
+      if (s > 10000) {
+        return res.status(400).json({
+          message: "Invalid size parameter. size must be <=10000",
+        });
+      }
+    }
+
+    if (w) {
+      w = parseInt(w);
+      if (w > 10000) {
+        return res.status(400).json({
+          message: "Invalid width parameter. width must be <=10000",
+        });
+      }
+    }
+
+    if (h) {
+      h = parseInt(h);
+      if (h > 10000) {
+        return res.status(400).json({
+          message: "Invalid height parameter. height must be <=10000",
+        });
+      }
+    }
+
+    if (q) {
+      q = parseInt(q);
+      if (q > 100) {
+        return res.status(400).json({
+          message: "Invalid quality parameter. quality must be <=10000",
+        });
+      }
+    }
+
+    res.setHeader("content-type", "image/jpg");
+    const image = sharp(`./public/${filename}`).toFormat("jpeg", {
+      quality: q ? parseInt(q) : 60,
+    });
+
+    res.send(
+      await image
+        .resize(
+          s
+            ? s
+            : h && !w
+            ? (
+                await image.metadata()
+              ).width
+            : w
+            ? parseInt(w)
+            : null,
+          s
+            ? s
+            : w && !h
+            ? (
+                await image.metadata()
+              ).height
+            : h
+            ? parseInt(h)
+            : null,
+          { fit, background: bg ? "#" + bg : null }
+        )
+        .withMetadata()
+        .toBuffer()
+    );
+  } catch (error) {
+    console.log(error);
+    res.setHeader("content-type", "application/json");
+    res
+      .status(500)
+      .json({ message: "Internal server error", status: 500, success: false });
+  }
+});
+
+const uploadMiddleware = multer({
+  limits: 10_000_000,
+  storage: multer.diskStorage({
+    destination: "./public",
+    filename: function (req, file, cb) {
+      cb(null, v4() + path.extname(file.originalname));
+    },
+  }),
+}).single("file");
+
+app.post("/upload", uploadMiddleware, (req, res, next) => {
+  console.log(req.files);
+  res.json({
+    url: `http://localhost:5000/public/${req.file.filename}`,
+    message: "success",
+  });
+});
+
+app.get("/upload", uploadMiddleware, (req, res, next) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/", async (req, res, next) => {
+  const files = fs.readdirSync("./public");
+  res.json(
+    files.map((file) => ({ url: `http://localhost:5000/public/${file}` }))
+  );
+});
+
+const port = 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
